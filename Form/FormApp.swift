79673a9616b78b1c456f1fc459ab6875d6d9de35
AbhirathSP <abhirathsp2@gmail.com>
2022-07-13 00:35:01 +0530

    Initial Commit

---
+++ Form/FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormApp.swift
+//  Form
+//
+//  Created by Abhirath Sujith on 13/07/22.
+//
+
+import SwiftUI
+
+@main
+struct FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
