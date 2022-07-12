5eeda295206a7e7a1e516cc2d298262315addeeb
AbhirathSP <abhirathsp2@gmail.com>
2022-07-13 02:14:10 +0530

    Update ContentView.swift

--- Form/ContentView.swift
+++ Form/ContentView.swift
@@ -8,10 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var firstname = ""
+    @State private var lastname = ""
+    @State private var DOB = Date()
+    @State private var send = false
+    @State private var vote = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form{
+                Section(header: Text("Personal Details")) {
+                    TextField("First Name", text: $firstname)
+                    TextField("Last Name", text: $lastname)
+                    DatePicker("DOB", selection: $DOB, displayedComponents: .date)
+                }
+                
+                Section( header: Text("More Details")) {
+                    
+                    Toggle("Send Notifications", isOn: $send)
+                       // .toggleStyle(SwitchToggleStyle(tint: .mint))
+                    
+                    
+                    Stepper("Number of Votes", value: $vote, in: 1...50)
+                    Text("Casted Votes are \(vote)")
+                }
+                
+                
+            }
+            
+            .navigationTitle("Form")
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    
+                    Button {
+                        hidekey()
+                    } label: {
+                        Image(systemName: "keyboard")
+                    }
+                    
+                    Button("Vote", action: votec)
+                }
+            }
+            
+        }
+       
+        
+        
     }
+    
+    func votec() {
+        print("Casted vote")
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -19,3 +68,14 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+#if canImport(UIKit)
+extension View {
+    
+    func hidekey() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+    
+}
+
+#endif
